019fa159a560d02110c6de9edbf271c07796f6f5
WilsonMites <wilsonmites1@gmail.com>
2025-11-29 23:23:48 -0500

    Trabajo listo para poder capturar los products

--- src/main/kotlin/com/pucetec/products/models/entities/Invoice.kt
+++ src/main/kotlin/com/pucetec/products/models/entities/Invoice.kt
@@ -2,25 +2,31 @@ package com.pucetec.products.models.entities
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
+import jakarta.persistence.Transient
 
 @Entity
 @Table(name = "invoices")
 data class Invoice (
     @Column(name = "client_ci")
     val clientCi: String,
+
     @Column(name = "client_name")
     val clientName: String,
+
     @Column(name = "client_address")
     val clientAddress: String,
+
     @Column(name = "total_before_taxes")
     val totalBeforeTaxes: Float? = null,
+
     val taxes: Float? = null,
+
     @Column(name = "total_after_taxes")
     val totalAfterTaxes: Float? = null,
 
-    @OneToMany
-    val invoiceDetails: List<InvoiceDetail> = emptyList(),
+    // 🔹 Ya NO es @OneToMany, ahora es solo un campo de apoyo
+    @Transient
+    var invoiceDetails: MutableList<InvoiceDetail> = mutableListOf(),
 
-    ): BaseEntity()
+    ) : BaseEntity()
